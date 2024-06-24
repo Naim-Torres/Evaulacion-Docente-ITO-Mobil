80034233b310fe21da78d8defc849e2817af306d
Nizvan Monteón Ricárdez <njr01397@gmail.com>
2024-06-24 17:46:11 -0600

    Feat(reset_password): Add invalid token view

---
+++ src/app/components/email/InvalidToken.tsx
@@ -0,0 +1,17 @@
+import InvalidTokenIcon from "../icons/InvalidTokenIcon";
+
+export default function InvalidToken() {
+    return (
+        <main className="flex justify-center items-center w-full h-screen p-8 sm:p-16 bg-primary-100">
+            <div className="overflow-hidden flex flex-col items-center gap-12 w-full max-w-[500px] h-[600px] p-12 rounded-lg shadow-lg duration-300 hover:shadow-xl bg-white">
+                {/* ILLUSTRATION */}
+                <InvalidTokenIcon width={'100%'} height={'100%'} />
+
+                <div>
+                    <h1 className="title highlight mb-8sm:mb-0">Token inválido</h1>
+                    <p className="mt-4">Al parecer el token para recuperrar este correo no es correcto o ya expiró. Si necesitas restablecer tu contraseña accede a &quot;¿Olvidate tu contraseña?&quot; y sigue las instrucciones.</p>
+                </div>
+            </div>
+        </main>
+    )
+}
